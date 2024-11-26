0d0cfd7c053ec94bed4b979b91cc566c15aeb7f0
kimuijin <dmlwls2435@naver.com>
2024-11-26 21:51:08 +0900

    Initial commit

---
+++ src/test/java/com/study/oauth/KakaoRestapiLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoRestapiLoginApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
